d61bd4adcb2456b3adbff6cd7b440b8410805dfd
sinatayebati <sinatayebati@gmail.com>
2026-03-26 15:42:11 -0500

    updating dock string

--- libraries/python/src/pandaprobe/integrations/__init__.py
+++ libraries/python/src/pandaprobe/integrations/__init__.py
@@ -4,4 +4,5 @@ Each adapter lives in its own subpackage and should be imported directly::
 
     from pandaprobe.integrations.google_adk import GoogleADKAdapter
     from pandaprobe.integrations.langgraph import LangGraphCallbackHandler
+    from pandaprobe.integrations.claude_agent_sdk import ClaudeAgentSDKAdapter
 """
