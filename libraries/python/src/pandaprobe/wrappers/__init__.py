9e8fec2c44d7d0747476e5bcaeffb33bef397951
sinatayebati <sinatayebati@gmail.com>
2026-05-18 21:15:51 -0500

    minor update

--- libraries/python/src/pandaprobe/wrappers/__init__.py
+++ libraries/python/src/pandaprobe/wrappers/__init__.py
@@ -5,8 +5,16 @@ Adding a new provider only requires creating a new sub-package and
 re-exporting the ``wrap_*`` function here.
 """
 
-from pandaprobe.wrappers.openai import wrap_openai
-from pandaprobe.wrappers.gemini import wrap_gemini
 from pandaprobe.wrappers.anthropic import wrap_anthropic
+from pandaprobe.wrappers.bedrock import wrap_bedrock
+from pandaprobe.wrappers.gemini import wrap_gemini
+from pandaprobe.wrappers.mistral import wrap_mistral
+from pandaprobe.wrappers.openai import wrap_openai
 
-__all__ = ["wrap_openai", "wrap_gemini", "wrap_anthropic"]
+__all__ = [
+    "wrap_anthropic",
+    "wrap_bedrock",
+    "wrap_gemini",
+    "wrap_mistral",
+    "wrap_openai",
+]
